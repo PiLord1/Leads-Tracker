4efa80381f9d6c77eae400e20ab85006280c20f2
Jean Berin <51207250+PiLord1@users.noreply.github.com>
2024-08-26 15:14:06 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const saveInputBtn = document.getElementById('input-btn');
 const saveTabBtn = document.getElementById('saveTab-btn');
 const deleteAllBtn = document.getElementById('deleteAll-btn');
 let myLeads = [];
+let leadsLocalStorage = JSON.parse(localStorage.getItem('lead'));
 const inputEl = document.getElementById('input-el');
 const gatheredLeads = document.getElementById('gatheredLeads');
 const listLeads = document.createElement('ul');
@@ -11,57 +12,38 @@ saveInputBtn.addEventListener('click', function() {
     myLeads.push(inputEl.value);
     inputEl.value = '';
     localStorage.setItem('lead', JSON.stringify(myLeads));
-    myLeads = JSON.parse(localStorage.getItem('lead'));
-    
-   
-    renderLeads();
-    console.log(myLeads);
-
+    leadsLocalStorage = JSON.parse(localStorage.getItem('lead'));
+    renderLeads(myLeads);
 })
 
 saveTabBtn.addEventListener('click', function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
         myLeads.push(tabs[0].url);
         localStorage.setItem('lead', JSON.stringify(myLeads));
-        renderLeads();
-
+        leadsLocalStorage = JSON.parse(localStorage.getItem('lead'));
+        renderLeads(myLeads);
     })
 })
 
 deleteAllBtn.addEventListener('dblclick', function() {
     localStorage.clear();
-    renderLeads();
+    myLeads = [];
+    renderLeads(myLeads);
 })
 
-function renderLeads() {
-    if (localStorage.getItem('lead') === null) {
-        myLeads = [];
-
-    } else {
-        myLeads = JSON.parse(localStorage.getItem('lead'));
-
-    }
-
+function renderLeads(array) {
     listLeads.innerHTML = '';
 
-    for (i=0; i < myLeads.length; i++) {
-        listLeads.innerHTML += `<li><a href="${myLeads[i]}" target="_blank">${myLeads[i]}</a></li>`;
-    
-    
+    for (i=0; i < array.length; i++) {
+        listLeads.innerHTML += `<li><a href="${array[i]}" target="_blank">${array[i]}</a></li>`;
     }
     
     gatheredLeads.appendChild(listLeads);
-
 }
 
-if (localStorage.getItem('lead') === null) {
-
+if (localStorage.getItem('lead') !== null) {
+    myLeads = leadsLocalStorage;
+    renderLeads(leadsLocalStorage);
 } else {
-    renderLeads();
+    myLeads = [];
 }
-
-
-
-
-
-
